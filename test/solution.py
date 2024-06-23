cb3a25a33fa89a29220d14a20dd3451bb7fbf7dc
86158 <2410973613@qq.com>
2024-06-23 15:30:29 +0800

    新闻汇总

--- test/solution.py
+++ test/solution.py
@@ -1,11 +1,7 @@
-class Solution:
-    def oneEditAway(self, first: str, second: str) -> bool:
-        m, n = len(first), len(second)
-        if m < n:
-            return self.oneEditAway(second, first)
-        if m - n > 1:
-            return False
-        for i, (x, y) in enumerate(zip(first, second)):
-            if x != y:
-                return first[i + 1:] == second[i + 1:] if m == n else first[i + 1:] == second[i:]
-        return True
+class Test:
+    def __init__(self):
+        self.a = 1
+
+
+def test(self):
+    print(1)
