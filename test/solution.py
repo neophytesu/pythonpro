6bd3e433fe5a3f328d4becce3fa0e18c1b688977
86158 <2410973613@qq.com>
2024-07-01 13:09:14 +0800

    小游戏

--- test/solution.py
+++ test/solution.py
@@ -1,7 +1,30 @@
-class Test:
-    def __init__(self):
-        self.a = 1
+import copy
+import queue
+from collections import defaultdict
+from typing import List
 
 
-def test(self):
-    print(1)
+class Solution:
+    def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
+        g = defaultdict(list)
+        for x, y, z in edges:
+            g[x].append((y, z))
+            g[y].append((x, z))
+        visited = {0}
+        ans = 0
+
+        def dfs(u: int, time: int, value: int) -> None:
+            if u == 0:
+                nonlocal ans
+                ans = max(ans, value)
+            for v, dist in g[u]:
+                if time + dist <= maxTime:
+                    if v not in visited:
+                        visited.add(v)
+                        dfs(v, time + dist, value + values[v])
+                        visited.discard(v)
+                    else:
+                        dfs(v, time + dist, value)
+
+        dfs(0, 0, values[0])
+        return ans
